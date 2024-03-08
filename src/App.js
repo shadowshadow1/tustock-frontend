3e4a6d7ee51434c24057bb6ba0d9e96019ff545a
Joshua Angugo Ochieng <joshuaangugo@gmail.com>
2024-03-08 15:51:16 +0300

    initial routing successfully implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { Ecommerce, Investors, AdminProjects, Aggregators, Farmers, Lands, AdminLandLease, AdminChats } from './components';
+
+import { AggregatorHome, AggregatorFarmers, AggregatorLands, AggregatorLandLease, AggregatorProjects, AggregatorChats } from './components';
+
+import { HomePage, InvestorProjects, Investments, InvestorLands, InvestorLandLease, InvestorChats } from './components';
+
+import { LandingPage, OrderBook, Charts, Features } from './components';
+
+import { Button, Footer, Header, Navbar, NavChat, Notification, Sidebar, UserProfile } from './components';
+
 import './App.css';
 
 const App = () => {
@@ -30,35 +40,35 @@ const App = () => {
           <div>
             <Routes>
              {/* Admin dashboard */}
-              <Route path="/admindash" element="Ecommerce" />
-              <Route path="/invetors" element="Investors" />
-              <Route path="/projects" element="Projects" />
-              <Route path="/aggregators" element="Aggregators" />
-              <Route path='/farmers' element='Farmers' />
-              <Route path="/lands" element="Lands" />
-              <Route path="/landlease" element="LandLease" />
-              <Route path="/chats" element="Chats" />
+              <Route path="/admindash" element={<Ecommerce />}/>
+              <Route path="/invetors" element={<Investors />}/>
+              <Route path="/projects" element={<AdminProjects />} />
+              <Route path="/aggregators" element={<Aggregators />} />
+              <Route path='/farmers' element={<Farmers />} />
+              <Route path="/lands" element={<Lands />} />
+              <Route path="/landlease" element={<AdminLandLease />} />
+              <Route path="/chats" element={<AdminChats />} />
 
               {/* Pages */}
-              <Route path='/' element='LandingPage' />
-              <Route path="/oderbook" element="OrderBook" />
-              <Route path='/charts' element="Charts" />
-              <Route path='/features' element="Features" />
+              <Route path='/' element={<LandingPage />} />
+              <Route path="/oderbook" element={<OrderBook />} />
+              <Route path='/charts' element={<Charts />} />
+              <Route path='/features' element={<Features />} />
 
               {/* Aggregator dashboard */}
-              <Route path='/aggregatorHome' element='AggregatorHome' />
-              <Route path='/farmers' element='Farmers' />
-              <Route path="/lands" element="Lands" />
-              <Route path='/projects' element='Projects' />
-              <Route path="/chats" element="Chats" />
+              <Route path='/aggregatorHome' element={<AggregatorHome />} />
+              <Route path='/farmers' element={<AggregatorFarmers />} />
+              <Route path="/lands" element={<AggregatorLands />} />
+              <Route path='/projects' element={<AggregatorProjects />} />
+              <Route path="/chats" element={<AggregatorChats />} />
               
               {/* Investors */}
-              <Route path='/investor' element='HomePage' />
-              <Route path='/projects' element='Projects' />
-              <Route path='/investments' element='Investments' />
-              <Route path='/lands' element='Lands' />
-              <Route path='/landlease' element='LandLease' />
-              <Route path='/chats' element='Chats' />
+              <Route path='/investor' element={<HomePage />} />
+              <Route path='/projects' element={<InvestorProjects />} />
+              <Route path='/investments' element={<Investments />} />
+              <Route path='/lands' element={<InvestorLands />} />
+              <Route path='/landlease' element={<InvestorLandLease />} />
+              <Route path='/chats' element={<InvestorChats />}/>
 
             </Routes>
           </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 // Admin exports
 export { default as Ecommerce } from './admin/Ecommerce';
 export { default as Investors } from './admin/Investors';
-export { default as Projects } from './admin/Projects';
+export { default as AdminProjects } from './admin/Projects';
 export { default as Aggregators } from './admin/Aggregators';
 export { default as Farmers } from './admin/Farmers';
 export { default as Lands } from './admin/Lands';
@@ -10,17 +10,17 @@ export { default as AdminChats } from './admin/Chats';
 
 // Aggregator exports
 export { default as AggregatorHome } from './aggregator/AggregatorHome';
-export { default as Farmers } from './aggregator/Farmers';
-export { default as Lands } from './aggregator/Lands';
+export { default as AggregatorFarmers } from './aggregator/Farmers';
+export { default as AggregatorLands } from './aggregator/Lands';
 export { default as AggregatorLandLease } from './admin/LandLease';
-export { default as Projects } from './aggregator/Projects';
+export { default as AggregatorProjects } from './aggregator/Projects';
 export { default as AggregatorChats } from './aggregator/Chats';
 
 // Investor exports
 export { default as HomePage } from './investor/HomePage';
-export { default as Projects } from './investor/Projects';
+export { default as InvestorProjects } from './investor/Projects';
 export { default as Investments } from './investor/Investments';
-export { default as AggregatorLands } from './investor/Lands';
+export { default as InvestorLands } from './investor/Lands';
 export { default as InvestorLandLease } from './investor/LandLease';
 export { default as InvestorChats } from './investor/Chats';
 
