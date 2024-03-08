46cedd45bd6e32df1992f57751b21f9885706b25
Joshua Angugo Ochieng <joshuaangugo@gmail.com>
2024-03-08 07:46:17 +0300

    initial sets of routes established

--- src/App.js
+++ src/App.js
@@ -8,22 +8,63 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        {activeMenu ? (
-          <div className='sidebar' >
-            Sidebar
-          </div>
-        ): (
-          <div className='sidebar-hidden' >
-            Sidebar hidden
-          </div>
-        ) }
-
-        <div className={`main-content ${activeMenu ? 'margin-left' : 'flex-2'}`}>
-            <div className='navbar'>
-                Navbar
+        <div className='main'>
+          {activeMenu ? (
+            <div className='sidebar' >
+              Sidebar
             </div>
+          ): (
+            <div className='sidebar-hidden' >
+              Sidebar hidden
+            </div>
+          ) }
+
+          <div className={`main-content ${activeMenu ? 'margin-left' : 'flex-2'}`}>
+              <div className='navbar'>
+                  Navbar
+              </div>
+          </div>
+
+
+
+          <div>
+            <Routes>
+             {/* Admin dashboard */}
+              <Route path="/admindash" element="Ecommerce" />
+              <Route path="/invetors" element="Investors" />
+              <Route path="/projects" element="Projects" />
+              <Route path="/aggregators" element="Aggregators" />
+              <Route path='/farmers' element='Farmers' />
+              <Route path="/lands" element="Lands" />
+              <Route path="/chats" element="Chat" />
+
+              {/* Pages */}
+              <Route path='/' element='LandingPage' />
+              <Route path="/oderbook" element="OrderBook" />
+              <Route path='/charts' element="Chart" />
+              <Route path='/features' element="Features" />
+
+              {/* Aggregator dashboard */}
+              <Route path='/aggregatorDash' element='AggregatorDash' />
+              <Route path='/farmers' element='Farmers' />
+              <Route path="/lands" element="Lands" />
+              <Route path='/projects' element='Projects' />
+              <Route path="/chats" element="Chat" />
+              
+              {/* Investors */}
+              <Route path='/investor' element='HomePage' />
+              <Route path='/projects' element='Projects' />
+              <Route path='/investments' element='Investments' />
+              <Route path='/lands' element='Lands' />
+              <Route path='/landlease' element='LandLease' />
+              <Route path='/chats' element='Chats' />
+
+            </Routes>
+          </div>
+
+
+
         </div>
-        
       </BrowserRouter>
     </div>
   )
