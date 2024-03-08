8cc9431831e1dfe4ce9a22a13316d1df925b48af
Joshua Angugo Ochieng <joshuaangugo@gmail.com>
2024-03-08 07:02:27 +0300

    sidebar and navbar place holders added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './App.css';
 
 const App = () => {
+  const activeMenu=true;
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        {activeMenu ? (
+          <div className='sidebar' >
+            Sidebar
+          </div>
+        ): (
+          <div className='sidebar-hidden' >
+            Sidebar hidden
+          </div>
+        ) }
+
+        <div className={`main-content ${activeMenu ? 'margin-left' : 'flex-2'}`}>
+            <div className='navbar'>
+                Navbar
+            </div>
+        </div>
+        
+      </BrowserRouter>
+    </div>
   )
 }
 
