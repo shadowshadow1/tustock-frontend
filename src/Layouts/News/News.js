f219f0bca2b40f0f5403bba42d9f75ae3265a3c2
Joshua Ochieng <126759220+angugo@users.noreply.github.com>
2024-04-01 12:48:39 +0300

    news section added

---
+++ src/Layouts/News/News.js
@@ -0,0 +1,13 @@
+import styles from "./News.module.css";
+import Image from "next/image";
+
+const News = () => {
+    return (
+        <div className={styles.news}>
+           
+            
+        </div>
+    );
+}
+
+export default News;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,14 @@
 import Header from '@/Layouts/Header/Header'
 import styles from './page.module.css'
 import NavBar from '@/Layouts/NavBar/NavBar'
+import News from '@/Layouts/News/News'
 export default function Home() {
   return (
     <>
     <NavBar/>
     <Header/>
+    <News/>
+
     </>
   )
 }
